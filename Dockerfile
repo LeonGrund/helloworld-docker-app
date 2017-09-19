53d06e57bd85583c5009ac7d5795ca1af1f7d939
Leon Grund <leoncapgrund@gmail.com>
2017-09-18 23:35:59 -0500

    jaa!

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 FROM elyase/staticpython
 
 COPY test.py /run
-# requirements.txt /run/
-
-# pip install --requirement /run/requirements.txt
-
-EXPOSE 80:8080
 
 CMD [ "python", "run/test.py" ]
--- test.py
+++ test.py
@@ -1,9 +1,31 @@
+import sys
+import socket
+import select
+import struct
 import time
-print("\n***D O C K E R  T E S T  A P P  S T A R T E D***\n")
-print("   HELLO WORLD !")
-ct = 0
 
-while True:
-    print("   Running for " + str(ct) + " minutes\n")
-    ct += 1
-    time.sleep(60)
+
+print("\n***D O C K E R  T E S T  A P P  S T A R T E D***\n")
+
+ip = '0.0.0.0'
+port = 4040
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((ip, port))
+#s.connect((ip, port))
+s.listen(1)
+
+def helloworld(errorNum, errorType):
+    header = 'HTTP/1.1 %d %s \r\nContent-Type: text/html\n' % (errorNum, errorType)
+    body = '<html>\r\n\t<head>\r\n\t\t<title>Docker-App\r\n\t\t</title>\r\n\t</head>\r\n\r\n<body>\r\n\tHello World!\r\n</body>\r\n</html>'
+    msg = header + body + '\r\n\r\n'
+    return msg
+
+print('Hello World App is listening on %s:%d' % (ip, port))
+
+while 1:
+    (clientsocket, address) = s.accept()
+    toSend = helloworld(200, 'OK')
+    clientsocket.send(toSend.encode())
+    clientsocket.shutdown(1)
+    clientsocket.close()
